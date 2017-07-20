909fb25607780861d44575e99f3b0f0640867bf5
Oluwaseun Makinde <oluwaseunmakinde@v1020-wn-108-246.campus-dynamic.uwaterloo.ca>
2017-07-20 14:25:52 -0400

    login and sign up

--- ChemoTracker/SignUpViewController.swift
+++ ChemoTracker/SignUpViewController.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController, UITextFieldDelegate{
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.nameField.delegate = self
+        self.emailField.delegate = self
+        self.passwordField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.nameField.resignFirstResponder()
+        self.emailField.resignFirstResponder()
+        self.passwordField.resignFirstResponder()
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
+    
+    @IBAction func signUpAction(_ sender: AnyObject) {
+        //        var finalEmail = email.stringByTrimmi
+    }
+    
+}
+
