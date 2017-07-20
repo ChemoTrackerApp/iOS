f9a5e4171380c719804f0d58da7101bcac52b830
Oluwaseun Makinde <oluwaseunmakinde@v1020-wn-108-246.campus-dynamic.uwaterloo.ca>
2017-07-20 14:24:28 -0400

    login and sign up

---
+++ ChemoTracker/SignUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpViewController.swift
+//  ChemoTracker
+//
+//  Created by Oluwaseun Makinde on 2017-07-20.
+//  Copyright © 2017 Innovare. All rights reserved.
+//
+
+import Foundation
